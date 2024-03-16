b729c64ddeb50aafde7190e28577c85c7b92e769
starfrost013 <mario64crashed@gmail.com>
2024-03-16 01:02:57 +0000

    made it compile on both MSVC (Visual Studio solution) and GCC/MinGW (MSVC doesn't work yet...)

--- CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/mario/Zombono/tools/zbsp
-# Build directory: C:/Users/mario/Zombono/tools/zbsp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(q2tool "C:/Users/mario/Zombono/tools/zbsp/q2tool.exe" "-bsp")
-set_tests_properties(q2tool PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/mario/Zombono/tools/zbsp/CMakeLists.txt;79;add_test;C:/Users/mario/Zombono/tools/zbsp/CMakeLists.txt;0;")
